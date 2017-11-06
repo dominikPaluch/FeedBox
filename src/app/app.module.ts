f4a9bd300e81380853a3199e0ec8f277d08a9715
Dominik Paluch <dominik-paluch@o2.pl>
2017-11-06 15:01:33 +0100

    add slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { OpinionComponent } from './components/opinion/opinion.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {OpinionComponent} from './components/opinion/opinion.component';
+
+import {CarouselModule, TooltipModule} from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    OpinionComponent
+    OpinionComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/opinion/opinion.component.ts
+++ src/app/components/opinion/opinion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Animal} from "../../models/Animals";
 
 @Component({
   selector: 'app-opinion',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpinionComponent implements OnInit {
 
-  constructor() { }
+  animals: Animal[];
+
+  constructor() {
+    this.animals = [
+      {type: 'Lew afrykański', name: 'Zbyszek', photo: 'lion', description: 'Lubi kasze gryczaną z antylopkami'},
+      {type: 'Żyrafa', name: 'Elma', photo: 'giraffe', description: 'Ma długą szyję i lęk wysokości. Płochliwa'},
+      {type: 'Słoń', name: 'Janusz', photo: 'elephant', description: 'Lubi jeść wszystko. Z Zahirka szczególnie'},
+      {type: 'Żyrafa', name: 'Elma', photo: 'giraffe', description: 'Ma długą szyję i lęk wysokości. Płochliwa'},
+      {type: 'Lew afrykański', name: 'Zbyszek', photo: 'lion', description: 'Lubi kasze gryczaną z antylopkami'},
+      {type: 'Pingwiny', name: 'Z Madagaskaru', photo: 'penguins', description: 'Kowalski, Szeregowy, Rico i Szefu'},
+      {type: 'Słoń', name: 'Janusz', photo: 'elephant', description: 'Lubi jeść wszystko. Z Zahirka szczególnie'},
+      {type: 'Pingwiny', name: 'Z Madagaskaru', photo: 'penguins', description: 'Kowalski, Szeregowy, Rico i Szefu'},
+      {type: 'Lew afrykański', name: 'Zbyszek', photo: 'lion', description: 'Lubi kasze gryczaną z antylopkami'},
+    ];
+  }
 
   ngOnInit() {
   }
