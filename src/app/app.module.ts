76ce25f7f74759c9a070875a8d78f28e85cbd755
Dominik Paluch <dominik-paluch@o2.pl>
2017-11-06 13:48:38 +0100

    add opinion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { OpinionComponent } from './components/opinion/opinion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    OpinionComponent
   ],
   imports: [
     BrowserModule
