7fd8801acf622de5e939d274143e2870aee43989
Krystian Szabat <flasher49@gmail.com>
2017-11-08 00:42:53 +0100

    Contact v1, menu, parallax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {AppComponent} from './app.component';
-import {HomeComponent} from './components/home/home.component';
-import {OpinionComponent} from './components/opinion/opinion.component';
-import {HeadersliderComponent} from './components/headerslider/headerslider.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { OpinionComponent } from './components/opinion/opinion.component';
+import { HeadersliderComponent } from './components/headerslider/headerslider.component';
 
-import {CarouselModule, TooltipModule} from 'primeng/primeng';
-import {Ng2PageScrollModule} from 'ng2-page-scroll';
-import {Ng2ScrollimateModule} from 'ng2-scrollimate';
+import { CarouselModule, TooltipModule } from 'primeng/primeng';
+import { Ng2PageScrollModule } from 'ng2-page-scroll';
+import { Ng2ScrollimateModule } from 'ng2-scrollimate';
+import { ContactComponent } from './components/contact/contact.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { BannerComponent } from './components/banner/banner.component';
+
+export const ROUTES: Routes = [
+  {
+    path: 'kontakt',
+    component: ContactComponent,
+    data: {
+      title: 'FoodBox - Kontakt'
+    }
+  }, {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     OpinionComponent,
-    HeadersliderComponent
+    HeadersliderComponent,
+    ContactComponent,
+    MenuComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +46,7 @@ import {Ng2ScrollimateModule} from 'ng2-scrollimate';
     TooltipModule,
     Ng2PageScrollModule,
     Ng2ScrollimateModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,59 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-import {animate, state, style, transition, trigger} from "@angular/animations";
-import {EasingLogic} from "ng2-page-scroll";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger("elementState", [
-      state("inactive", style({
-        opacity: 0,
-        visibility: "hidden",
-      })),
-      state("active", style({
-        opacity: 1,
-        visibility: "visible",
-      })),
-      transition("* => active", animate("300ms ease-in")),
-      transition("* => inactive", animate("300ms ease-out")),
-    ])
-  ]
 })
 export class HomeComponent implements OnInit {
 
-  scrollimateOptions: any = {
-    myScrollimation1: {
-      currentState: "inactive",
-      states: [
-        {
-          method: "pxElement",
-          value: 550,
-          state: "active",
-        },
-        {
-          method: "default",
-          state: "inactive"
-        }
-      ]
-    }
-  };
-
-  myEasing: EasingLogic = {
-    ease: (t: number, b: number, c: number, d: number): number => {
-      if (t === 0) {
-        return b;
-      }
-      if (t === d) {
-        return b + c;
-      }
-      if ((t /= d / 2) < 1) {
-        return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
-      }
-      return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
-    }
-  };
+  
 
   constructor() {
   }
