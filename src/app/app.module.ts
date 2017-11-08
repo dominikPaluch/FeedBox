e43ef1eddb3e31dbb0a7c643f5da08c86a9e6f80
Dominik Paluch <dominik-paluch@o2.pl>
2017-11-08 16:31:56 +0100

    add map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { OpinionComponent } from './components/opinion/opinion.component';
-import { HeadersliderComponent } from './components/headerslider/headerslider.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {OpinionComponent} from './components/opinion/opinion.component';
+import {HeadersliderComponent} from './components/headerslider/headerslider.component';
 
-import { CarouselModule, TooltipModule } from 'primeng/primeng';
-import { Ng2PageScrollModule } from 'ng2-page-scroll';
-import { Ng2ScrollimateModule } from 'ng2-scrollimate';
-import { ContactComponent } from './components/contact/contact.component';
-import { Routes, RouterModule } from '@angular/router';
-import { MenuComponent } from './components/menu/menu.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
+import {CarouselModule, TooltipModule} from 'primeng/primeng';
+import {Ng2PageScrollModule} from 'ng2-page-scroll';
+import {Ng2ScrollimateModule} from 'ng2-scrollimate';
+import {ContactComponent} from './components/contact/contact.component';
+import {RouterModule, Routes} from '@angular/router';
+import {MenuComponent} from './components/menu/menu.component';
+import {BannerComponent} from './components/banner/banner.component';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {MapComponent} from './components/map/map.component';
+import {AgmCoreModule} from '@agm/core';
 
 export const ROUTES: Routes = [
   {
@@ -39,7 +41,8 @@ export const ROUTES: Routes = [
     ContactComponent,
     MenuComponent,
     BannerComponent,
-    ProductListComponent
+    ProductListComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
@@ -48,7 +51,10 @@ export const ROUTES: Routes = [
     TooltipModule,
     Ng2PageScrollModule,
     Ng2ScrollimateModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDLHDTuibT_U4P15XvfzQ32Xys0tY3KLn8'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
