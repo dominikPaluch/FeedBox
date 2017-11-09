b32acae9c8bb0dfb31043c5f1e91ee8a0a46c520
Krystian Szabat <flasher49@gmail.com>
2017-11-09 21:45:05 +0100

    Products list, products page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {AppComponent} from './app.component';
-import {HomeComponent} from './components/home/home.component';
-import {OpinionComponent} from './components/opinion/opinion.component';
-import {HeadersliderComponent} from './components/headerslider/headerslider.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { OpinionComponent } from './components/opinion/opinion.component';
+import { HeadersliderComponent } from './components/headerslider/headerslider.component';
 
-import {CarouselModule, TooltipModule} from 'primeng/primeng';
-import {Ng2PageScrollModule} from 'ng2-page-scroll';
-import {Ng2ScrollimateModule} from 'ng2-scrollimate';
-import {ContactComponent} from './components/contact/contact.component';
-import {RouterModule, Routes} from '@angular/router';
-import {MenuComponent} from './components/menu/menu.component';
-import {BannerComponent} from './components/banner/banner.component';
-import {ProductListComponent} from './components/product-list/product-list.component';
-import {MapComponent} from './components/map/map.component';
-import {AgmCoreModule} from '@agm/core';
+import { CarouselModule, TooltipModule } from 'primeng/primeng';
+import { Ng2PageScrollModule } from 'ng2-page-scroll';
+import { Ng2ScrollimateModule } from 'ng2-scrollimate';
+import { ContactComponent } from './components/contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { MapComponent } from './components/map/map.component';
+import { AgmCoreModule } from '@agm/core';
 import { FooterComponent } from './components/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
 
 export const ROUTES: Routes = [
   {
@@ -25,6 +26,12 @@ export const ROUTES: Routes = [
     component: ContactComponent,
     data: {
       title: 'FoodBox - Kontakt'
+    }
+  }, {
+    path: 'produkty',
+    component: ProductsComponent,
+    data: {
+      title: 'FoodBox - Produkty'
     }
   }, {
     path: '',
@@ -44,7 +51,8 @@ export const ROUTES: Routes = [
     BannerComponent,
     ProductListComponent,
     MapComponent,
-    FooterComponent
+    FooterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {Product} from "../../models/Product";
-import {TypeOfProductEnum} from "../../models/typeOfProductEnum";
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from "../../models/Product";
+import { TypeOfProductEnum } from "../../models/typeOfProductEnum";
 
 @Component({
   selector: 'app-product-list',
@@ -9,55 +9,67 @@ import {TypeOfProductEnum} from "../../models/typeOfProductEnum";
 })
 export class ProductListComponent implements OnInit {
 
+  @Input() nums: number;
+
   products: Product[];
+  copyProducts: Product[];
   id = 1;
   counterCart = 0;
 
   constructor() {
-    this.initData();
   }
 
   ngOnInit() {
+    console.log('aaa')
+    this.initData();
   }
 
   initData() {
     this.products = [
-      {name: 'Mix owoców', photo: 'inner1.png', price: '9.99 zł', type: TypeOfProductEnum.FRUITS},
-      {name: 'Zestaw 1', photo: 'box10.png', price: '12.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Zestaw 2', photo: 'box11.png', price: '13.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Marchewka', photo: 'product-5.jpg', price: '6.99 zł', type: TypeOfProductEnum.VEGETABLES},
-      {name: 'Mango', photo: 'product-6.jpg', price: '12.99 zł', type: TypeOfProductEnum.FRUITS},
-      {name: 'Pomidor', photo: 'product-7.jpg', price: '9.99 zł', type: TypeOfProductEnum.VEGETABLES},
-      {name: 'Zestaw 3', photo: 'box12.png', price: '17.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Zestaw 4', photo: 'box8.png', price: '9.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Zestaw 5', photo: 'box2.png', price: '11.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Zestaw 6', photo: 'box3.png', price: '8.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Zestaw 7', photo: 'box4.png', price: '7.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Rzodkiewka', photo: 'product-2.jpg', price: '9.99 zł', type: TypeOfProductEnum.VEGETABLES},
-      {name: 'Granat', photo: 'product-1.jpg', price: '19.99 zł', type: TypeOfProductEnum.FRUITS},
-      {name: 'Zestaw 8', photo: 'box5.png', price: '14.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Zestaw 9', photo: 'box9.png', price: '21.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Zestaw 10', photo: 'box6.png', price: '11.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Zestaw 11', photo: 'box7.png', price: '7.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Zestaw 12', photo: 'box1.png', price: '8.99 zł', type: TypeOfProductEnum.FOODBOX},
-      {name: 'Zestaw 13', photo: 'glry1.png', price: '7.99 zł', type: TypeOfProductEnum.FRUITS},
-      {name: 'Kiwi', photo: 'glry2.png', price: '3.99 zł', type: TypeOfProductEnum.FRUITS},
-      {name: 'Cebula', photo: 'glry3.png', price: '1.99 zł', type: TypeOfProductEnum.VEGETABLES},
-      {name: 'Czereśnia', photo: 'glry4.png', price: '16.99 zł', type: TypeOfProductEnum.FRUITS},
-      {name: 'Steak', photo: 'beef1.png', price: '16.99 zł', type: TypeOfProductEnum.BEEF},
-      {name: 'Mięso dobre', photo: 'beef2.png', price: '16.99 zł', type: TypeOfProductEnum.BEEF},
-      {name: 'Kapusta', photo: 'product-3.jpg', price: '4.99 zł', type: TypeOfProductEnum.VEGETABLES},
-      {name: 'Kukurydza', photo: 'product-4.jpg', price: '3.99 zł', type: TypeOfProductEnum.VEGETABLES},
-      {name: 'Borówka', photo: 'product-8.png', price: '5.99 zł', type: TypeOfProductEnum.FRUITS},
-      {name: 'Sok marchewkowy', photo: 'juice2.png', price: '5.99 zł', type: TypeOfProductEnum.JUICES},
-      {name: 'Grejfrut', photo: 'inner2.png', price: '6.99 zł', type: TypeOfProductEnum.FRUITS},
-      {name: 'Kefir', photo: 'milk2.png', price: '2.99 zł', type: TypeOfProductEnum.MILKS},
-      {name: 'Jogurt naturalny', photo: 'milk1.png', price: '3.99 zł', type: TypeOfProductEnum.MILKS},
+      { name: 'Mix owoców', photo: 'inner1.png', price: '9.99 zł', type: TypeOfProductEnum.FRUITS },
+      { name: 'Zestaw 1', photo: 'box10.png', price: '12.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Zestaw 2', photo: 'box11.png', price: '13.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Marchewka', photo: 'product-5.jpg', price: '6.99 zł', type: TypeOfProductEnum.VEGETABLES },
+      { name: 'Mango', photo: 'product-6.jpg', price: '12.99 zł', type: TypeOfProductEnum.FRUITS },
+      { name: 'Pomidor', photo: 'product-7.jpg', price: '9.99 zł', type: TypeOfProductEnum.VEGETABLES },
+      { name: 'Zestaw 3', photo: 'box12.png', price: '17.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Zestaw 4', photo: 'box8.png', price: '9.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Zestaw 5', photo: 'box2.png', price: '11.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Zestaw 6', photo: 'box3.png', price: '8.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Zestaw 7', photo: 'box4.png', price: '7.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Rzodkiewka', photo: 'product-2.jpg', price: '9.99 zł', type: TypeOfProductEnum.VEGETABLES },
+      { name: 'Granat', photo: 'product-1.jpg', price: '19.99 zł', type: TypeOfProductEnum.FRUITS },
+      { name: 'Zestaw 8', photo: 'box5.png', price: '14.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Zestaw 9', photo: 'box9.png', price: '21.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Zestaw 10', photo: 'box6.png', price: '11.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Zestaw 11', photo: 'box7.png', price: '7.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Zestaw 12', photo: 'box1.png', price: '8.99 zł', type: TypeOfProductEnum.FOODBOX },
+      { name: 'Zestaw 13', photo: 'glry1.png', price: '7.99 zł', type: TypeOfProductEnum.FRUITS },
+      { name: 'Kiwi', photo: 'glry2.png', price: '3.99 zł', type: TypeOfProductEnum.FRUITS },
+      { name: 'Cebula', photo: 'glry3.png', price: '1.99 zł', type: TypeOfProductEnum.VEGETABLES },
+      { name: 'Czereśnia', photo: 'glry4.png', price: '16.99 zł', type: TypeOfProductEnum.FRUITS },
+      { name: 'Steak', photo: 'beef1.png', price: '16.99 zł', type: TypeOfProductEnum.BEEF },
+      { name: 'Mięso dobre', photo: 'beef2.png', price: '16.99 zł', type: TypeOfProductEnum.BEEF },
+      { name: 'Kapusta', photo: 'product-3.jpg', price: '4.99 zł', type: TypeOfProductEnum.VEGETABLES },
+      { name: 'Kukurydza', photo: 'product-4.jpg', price: '3.99 zł', type: TypeOfProductEnum.VEGETABLES },
+      { name: 'Borówka', photo: 'product-8.png', price: '5.99 zł', type: TypeOfProductEnum.FRUITS },
+      { name: 'Sok marchewkowy', photo: 'juice2.png', price: '5.99 zł', type: TypeOfProductEnum.JUICES },
+      { name: 'Grejfrut', photo: 'inner2.png', price: '6.99 zł', type: TypeOfProductEnum.FRUITS },
+      { name: 'Kefir', photo: 'milk2.png', price: '2.99 zł', type: TypeOfProductEnum.MILKS },
+      { name: 'Jogurt naturalny', photo: 'milk1.png', price: '3.99 zł', type: TypeOfProductEnum.MILKS },
     ];
+
+    if (this.nums === 0) {
+
+    } else {
+      this.products = this.products.slice(0, this.nums);
+    }
+
+    this.copyProducts = JSON.parse(JSON.stringify(this.products));
   }
 
   getItem(product) {
-    console.log(product);
+
   }
 
   addToCart() {
@@ -68,22 +80,49 @@ export class ProductListComponent implements OnInit {
     this.counterCart--;
   }
 
+  getProductsNumber(id): boolean {
+    let productsFromCategory: Product[];
+
+    let productsCopy: Product[] = JSON.parse(JSON.stringify(this.copyProducts));
+
+    if (id === 2) {
+      productsFromCategory = productsCopy.filter((val, i) => val.type === TypeOfProductEnum.VEGETABLES);
+    } else if (id === 3) {
+      productsFromCategory = productsCopy.filter((val, i) => val.type === TypeOfProductEnum.FRUITS);
+    } else if (id === 4) {
+      productsFromCategory = productsCopy.filter((val, i) => val.type === TypeOfProductEnum.MILKS);
+    } else if (id === 5) {
+      productsFromCategory = productsCopy.filter((val, i) => val.type === TypeOfProductEnum.JUICES);
+    } else if (id === 6) {
+      productsFromCategory = productsCopy.filter((val, i) => val.type === TypeOfProductEnum.BEEF);
+    } else if (id === 7) {
+      productsFromCategory = productsCopy.filter((val, i) => val.type === TypeOfProductEnum.FOODBOX);
+    }
+
+    if (productsFromCategory.length === 0) {
+      return false;
+    }
+    return true;
+  }
+
   select(id) {
-    this.initData();
+    // this.initData();
     this.id = id;
 
     if (id === 2) {
-      this.products = this.products.filter((val, i) => val.type === TypeOfProductEnum.VEGETABLES);
+      this.products = this.copyProducts.filter((val, i) => val.type === TypeOfProductEnum.VEGETABLES);
     } else if (id === 3) {
-      this.products = this.products.filter((val, i) => val.type === TypeOfProductEnum.FRUITS);
+      this.products = this.copyProducts.filter((val, i) => val.type === TypeOfProductEnum.FRUITS);
     } else if (id === 4) {
-      this.products = this.products.filter((val, i) => val.type === TypeOfProductEnum.MILKS);
+      this.products = this.copyProducts.filter((val, i) => val.type === TypeOfProductEnum.MILKS);
     } else if (id === 5) {
-      this.products = this.products.filter((val, i) => val.type === TypeOfProductEnum.JUICES);
+      this.products = this.copyProducts.filter((val, i) => val.type === TypeOfProductEnum.JUICES);
     } else if (id === 6) {
-      this.products = this.products.filter((val, i) => val.type === TypeOfProductEnum.BEEF);
+      this.products = this.copyProducts.filter((val, i) => val.type === TypeOfProductEnum.BEEF);
     } else if (id === 7) {
-      this.products = this.products.filter((val, i) => val.type === TypeOfProductEnum.FOODBOX);
+      this.products = this.copyProducts.filter((val, i) => val.type === TypeOfProductEnum.FOODBOX);
+    } else if (id === 1) {
+      this.products = this.copyProducts;
     }
   }
 }
